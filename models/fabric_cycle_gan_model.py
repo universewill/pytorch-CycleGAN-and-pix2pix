41d53a8141a774c82b7eb7ea90d43fd429795eb4
universewill <universe.will@gmail.com>
2020-09-14 20:07:48 +0800

    add texture residual loss

--- models/fabric_cycle_gan_model.py
+++ models/fabric_cycle_gan_model.py
@@ -172,10 +172,13 @@ class FabricCycleGANModel(BaseModel):
             # G_A should be identity if real_B is fed: ||G_A(B) - B||
             self.idt_A_combine = self.netG_A(self.real_B)
             self.idt_A = self.idt_A_combine[:,0:3, :,:]
-            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt
+            self.idt_A_texture = self.idt_A_combine[:,3, :,:]
+            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * lambda_B * lambda_idt \
+                              + self.criterionGAN(self.idt_A_texture, False)* lambda_B * lambda_idt
             # G_B should be identity if real_A is fed: ||G_B(A) - A||
-            self.idt_B_combine = self.netG_B(self.real_A)
-            self.idt_B = self.idt_B_combine[:, 0:3, :, :]
+            zero_texture = torch.zeros_like(self.idt_A_texture)
+            self.real_A_with_fabric_texture = torch.cat((self.real_A, zero_texture), 1)
+            self.idt_B = self.netG_B(self.real_A_with_fabric_texture)
             self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * lambda_A * lambda_idt
         else:
             self.loss_idt_A = 0
